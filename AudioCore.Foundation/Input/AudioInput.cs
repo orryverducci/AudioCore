aa995fa399be8c8254a833587bcf79364b5a4641
Orry Verducci <orry@orryverducci.co.uk>
2016-10-30 19:32:36 +0000

    Restrict input bit depth to 64 bits and whole bytes

--- AudioCore.Foundation/Input/AudioInput.cs
+++ AudioCore.Foundation/Input/AudioInput.cs
@@ -4,6 +4,13 @@ namespace AudioCore.Foundation
 {
     public class AudioInput
     {
+        #region Private Fields
+        /// <summary>
+        /// The audio bit depth in bits
+        /// </summary>
+        private int _bitDepth;
+        #endregion
+
         #region Properties
         /// <summary>
         /// Gets the number of audio channels
@@ -21,7 +28,27 @@ namespace AudioCore.Foundation
         /// Gets the audio bit depth
         /// </summary>
         /// <value>The audio bit depth in bits</value>
-        public int BitDepth { get; protected set; }
+        public int BitDepth
+        {
+            get
+            {
+                return _bitDepth;
+            }
+            protected set
+            {
+                // Check value is valid
+                if (value > 64 || value < 8)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "Bit depth must be between 8 bit and 64 bit");
+                }
+                else if (value % 8 > 0)
+                {
+                    throw new ArgumentException("Bit depth must a multiple of 8", nameof(value));
+                }
+                // Set value
+                _bitDepth = value;
+            }
+        }
 
         /// <summary>
         /// Gets the latency on the audio in milliseconds
