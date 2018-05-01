a0cc4f733b66fab429f53deada7bb168f21d6995
Orry Verducci <orry@orryverducci.co.uk>
2018-05-01 19:14:42 +0100

    Require audio input properties to be greater than 0

--- AudioCore/Input/AudioInput.cs
+++ AudioCore/Input/AudioInput.cs
@@ -5,24 +5,81 @@ namespace AudioCore
 {
     public class AudioInput
     {
+        #region Private Fields
+        /// <summary>
+        /// The number of audio channels
+        /// </summary>
+        private int _channels;
+
+        /// <summary>
+        /// The audio sample rate in Hertz
+        /// </summary>
+        private int _sampleRate;
+
+        /// <summary>
+        /// The latency on the audio in milliseconds
+        /// </summary>
+        private int _latency;
+        #endregion
+
         #region Properties
         /// <summary>
         /// Gets the number of audio channels
         /// </summary>
         /// <value>The number of audio channels</value>
-        public int Channels { get; protected set; }
+        public int Channels
+        {
+            get
+            {
+                return _channels;
+            }
+            protected set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "The number of audio channels must be greater than 0");
+                }
+                _channels = value;
+            }
+        }
 
         /// <summary>
         /// Gets the audio sample rate in Hertz
         /// </summary>
         /// <value>The audio sample rate in Hertz</value>
-        public int SampleRate { get; protected set; }
+        public int SampleRate {
+            get
+            {
+                return _sampleRate;
+            }
+            protected set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "The sample rate must be greater than 0");
+                }
+                _sampleRate = value;
+            }
+        }
 
         /// <summary>
         /// Gets the latency on the audio in milliseconds
         /// </summary>
         /// <value>The audio latency</value>
-        public int Latency { get; protected set; }
+        public int Latency {
+            get
+            {
+                return _latency;
+            }
+            protected set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "The latency must be greater than 0");
+                }
+                _latency = value;
+            }
+        }
 
         /// <summary>
         /// Gets the current playback state
