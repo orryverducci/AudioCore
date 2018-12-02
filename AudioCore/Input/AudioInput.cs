f9c63beedb40b660fbf18b863d0c872499d5d608
Orry Verducci <orry@orryverducci.co.uk>
2018-12-02 20:18:35 +0000

    Remove redundant System.Theading.Tasks references

--- AudioCore/Input/AudioInput.cs
+++ AudioCore/Input/AudioInput.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading.Tasks;
 using AudioCore.Common;
 
 namespace AudioCore.Input
--- AudioCore/Output/AudioOutput.cs
+++ AudioCore/Output/AudioOutput.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using AudioCore.Common;
 using AudioCore.Input;
 
