fde1e60f677529cc4cd598f0d1bc824d9379e3a1
Orry Verducci <orry@orryverducci.co.uk>
2019-03-12 21:11:01 +0000

    Remove unused demo app lifestyle overrides

--- AudioCore.Demo/App.xaml.cs
+++ AudioCore.Demo/App.xaml.cs
@@ -12,20 +12,5 @@ namespace AudioCore.Demo
             InitializeComponent();
             MainPage = new MainPage();
         }
-
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
-        }
     }
 }
