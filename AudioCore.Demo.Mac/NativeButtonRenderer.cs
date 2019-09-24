e12d70709e474dfbf60e4774c41da4169d6eec85
Orry Verducci <orry@orryverducci.co.uk>
2019-09-24 16:46:54 +0100

    Add button custom renderer on macOS
    Buttons in Xamarin.Forms on macOS by default don't use the system standard style. This adds a custom renderer which enabled the system default rounded bezel style.

---
+++ AudioCore.Demo.Mac/NativeButtonRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AppKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+using AudioCore.Demo.Mac;
+
+[assembly: ExportRenderer(typeof(Button), typeof(NativeButtonRenderer))]
+namespace AudioCore.Demo.Mac
+{
+    /// <summary>
+    /// Provides a custom renderer which sets a rounded bezel style to buttons, providing them with a more native appearance.
+    /// </summary>
+    public class NativeButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null && Control != null)
+            {
+                Control.BezelStyle = NSBezelStyle.Rounded;
+            }
+        }
+    }
+}
